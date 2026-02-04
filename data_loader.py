5e0246d77e2ac4f4d3620275c5cb3d453e01e023
Muhammadbilal206 <63650308+Muhammadbilal206@users.noreply.github.com>
2026-02-04 23:50:40 +0500

    Add function to load configuration from JSON file

--- data_loader.py
+++ data_loader.py
@@ -1,1 +1,10 @@
+import csv
+import json
+import os
 
+def load_config(config_file_name = "config.json"):
+  try:
+    with open(config_file_name,'r') as f:
+      return json.load(f)
+  except FileNotFoundError:
+    return None
